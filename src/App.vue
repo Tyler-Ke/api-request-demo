4efb25f8d1bb9c171016fdf89be7d3a40cf9442b
Tyler Ke <tdkeytalt@gmail.com>
2025-08-05 18:25:49 -0500

    example of vanilla javascript fetch

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
    <div>API Calls</div>
+   <hr />
    <div v-for="user in userObj.users">
       <h2>{{ user.name }}</h2>
       <p>{{ user.email }}</p>
@@ -16,11 +17,19 @@ const userObj = reactive({
 });
 
 onMounted(() => {
-   axios
+   fetch("https://jsonplaceholder.typicode.com/todos/1/users")
+      .then((response) => response.json())
+      .then((data) => {
+         console.log(data);
+         userObj.users = data;
+      });
+
+   /*axios
       .get("https://jsonplaceholder.typicode.com/todos/1/users")
       .then((response) => {
          console.log(response.data);
          userObj.users = response.data;
       });
+   */
 });
 </script>
