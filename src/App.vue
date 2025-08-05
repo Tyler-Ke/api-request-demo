949cab154220f55556616912e553cd43651ec58f
Tyler Ke <tdkeytalt@gmail.com>
2025-08-05 18:22:16 -0500

    used axios to fetch fake users from jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,26 @@
-<template>Hello World</template>
+<template>
+   <div>API Calls</div>
+   <div v-for="user in userObj.users">
+      <h2>{{ user.name }}</h2>
+      <p>{{ user.email }}</p>
+      <hr />
+   </div>
+</template>
+
+<script setup>
+import { reactive, onMounted } from "vue";
+import axios from "axios";
+
+const userObj = reactive({
+   users: [],
+});
+
+onMounted(() => {
+   axios
+      .get("https://jsonplaceholder.typicode.com/todos/1/users")
+      .then((response) => {
+         console.log(response.data);
+         userObj.users = response.data;
+      });
+});
+</script>
